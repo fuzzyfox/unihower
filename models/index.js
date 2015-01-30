11c51e4b0fda2851ca6145e1e3cc283493974ce0
William Duyck <fuzzyfox0@gmail.com>
2015-01-30 02:51:52 +0000

    better loading of database resources

--- models/index.js
+++ models/index.js
@@ -12,7 +12,8 @@ var fs = require( 'fs' );
 var path = require( 'path' );
 var lodash = require( 'lodash' );
 var Sequelize = require( 'sequelize' );
-var debug = require( 'debug' )( 'sequelize' );
+var debugLib = require( 'debug' );
+var debug = debugLib( 'models' );
 
 /**
  * Models/ORM Exports
@@ -27,12 +28,16 @@ var debug = require( 'debug' )( 'sequelize' );
  * @return {Function}    When provided with an environment this returned function returns a usable ORM.
  */
 module.exports = function( env ) {
+  if( env.dbInstance ) {
+    return env.dbInstance;
+  }
+
   // create an object to return w/ the ORM
   var db = {};
 
   // get an instance of Sequelize
   var sequelize = new Sequelize( env.get( 'db_connection_uri' ), {
-    logging: debug
+    logging: debugLib( 'sequelize' )
   });
 
   // load all model definitions
@@ -40,6 +45,7 @@ module.exports = function( env ) {
     // filter file matches to remove dotfiles and this file
     return ( ( file.indexOf( '.' ) !== 0 ) && ( file !== 'index.js' ) );
   }).forEach( function( file ) {
+    debug( 'Loading model from %s', file );
     // add model to the ORM
     var model = sequelize.import( path.join( __dirname, file ) );
     db[ model.name ] = model;
@@ -52,9 +58,17 @@ module.exports = function( env ) {
     }
   });
 
-  // return a usable ORM w/ instance and library
-  return lodash.extend( {
+  debug( 'Models loaded: ', Object.keys( db ).join( ', ' ) );
+
+  // create a usable ORM w/ instance and library
+  db = lodash.extend( {
     sequelize: sequelize,
     Sequelize: Sequelize
   }, db );
+
+  // cache db on env
+  env.dbInstance = db;
+
+  // return db
+  return db;
 };
