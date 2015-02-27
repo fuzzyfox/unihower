b85f2f251523150f5fd883873ab31b2d77be4a41
William Duyck <fuzzyfox0@gmail.com>
2015-02-27 00:40:11 +0000

    only error when NOT an expected possible issue due to db.sync({force:true})

--- migrations/20150224021623-addParanoidTimestamps.js
+++ migrations/20150224021623-addParanoidTimestamps.js
@@ -2,22 +2,52 @@
 
 module.exports = {
   up: function( migration, DataTypes ) {
+    var taskMigration = migration.addColumn( 'Tasks', 'deletedAt', {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null
+    }).catch( function( err ) {
+      if( err.original.code !== 'ER_DUP_FIELDNAME' ) {
+        throw err;
+      }
+
+      migration.sequelize.options.logging( 'NOTICE: Migration not run on Tasks due to lack of need.' );
+      return this;
+    });
+
     return migration.addColumn( 'Topics', 'deletedAt', {
       type: DataTypes.DATE,
       allowNull: true,
       defaultValue: null
     }).then( function() {
-      return migration.addColumn( 'Tasks', 'deletedAt', {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: null
-      });
+      return taskMigration;
+    }).catch( function( err ) {
+      if( err.original.code !== 'ER_DUP_FIELDNAME' ) {
+        throw err;
+      }
+
+      migration.sequelize.options.logging( 'NOTICE: Migration not run on Topics due to lack of need.' );
+      return taskMigration;
     });
   },
 
   down: function( migration, DataTypes, done ) {
+    var taskMigration = migration.removeColumn( 'Tasks', 'deletedAt' ).catch( function( err ) {
+      if( err.original.code !== 'ER_CANT_DROP_FIELD_OR_KEY' ) {
+        throw err;
+      }
+
+      migration.sequelize.options.logging( 'WARNING: Migration not run on Tasks this should no be a problem however you may wish to manually remove database.Topics( `deletedAt` ).' );
+    });
+
     return migration.removeColumn( 'Topics', 'deletedAt' ).then( function() {
-      return migration.removeColumn( 'Tasks', 'deletedAt' );
+      return taskMigration;
+    }).catch( function( err ) {
+      if( err.original.code !== 'ER_CANT_DROP_FIELD_OR_KEY' ) {
+        throw err;
+      }
+
+      migration.sequelize.options.logging( 'WARNING: Migration not run on Topics this should no be a problem however you may wish to manually remove database.Topics( `deletedAt` ).' );
     });
   }
 };
